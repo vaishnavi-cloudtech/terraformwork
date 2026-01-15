b14e764c3dad206c860f3e0bfaadf133bfe8dc04
vaishnavi <misalvaishnavi2208@gmail.com>
2026-01-15 15:42:46 +0530

    Day-5

---
+++ Day-5-remote-backend-dynamodb-lock/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "vaishnavidevtest"
+    key    = "day-4/terraform.tfstate"
+    region = "us-east-1"
+    # Enable S3 native locking
+   # use_lockfile = true 
+    # The dynamodb_table argument is no longer needed
+
+     dynamodb_table = "terraform-state-lock-dynamo"
+
+  }
+}
